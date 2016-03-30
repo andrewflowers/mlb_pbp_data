62c16c8d7f39e5ae5a2eb540025bd44ff811c979
Andrew Flowers <andrew.w.flowers@gmail.com>
2016-03-30 17:36:34 -0400

    add 2015 data

--- download_retrosheet_data.R
+++ download_retrosheet_data.R
@@ -2,9 +2,10 @@
 
 ######### INSTRUCTIONS #########
 # Start here: https://baseballwithr.wordpress.com/2014/02/10/downloading-retrosheet-data-and-runs-expectancy/
+# Must first download Chadwick software. See here: http://www.pitchbypitch.com/2013/11/29/installing-chadwick-software-on-mac/
 ######### INSTRUCTIONS #########
 
-setwd("~/editing/mlb_pbp_data/")
+setwd("~/repos/mlb_pbp_data/")
 
 require(devtools)
 require(readr)
@@ -15,7 +16,7 @@ source_gist(8892981) # Downloads parse.retrosheet2.pbp function
 fields <- read_csv("fields.csv")
 headers <- fields$Header
 
-seasons <- rep(c(1925, 1927, 1930:2014)) # Seasons to download
+seasons <- rep(c(1925, 1927, 1930:2015)) # Seasons to download
 
 for (s in seasons){
   parse.retrosheet2.pbp(s)
